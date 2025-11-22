d808b0097c899f45c32b2ea0efe3920c60b524de
NaimaAfsanaNisha <bijoynisha143@gmail.com>
2025-11-23 03:03:44 +0600

    frontend mastering done

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Models\Task;
+use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Web\Dashboard\DashboardController;
-
-use Illuminate\support\Facades\Route;
 
 
-Route::get('/', [DashboardController::class, 'dashboard']);
-Route::get('/blog', [DashboardController::class, 'blog']);
+Route::get('/dashboard', function () {
+    $user = auth()->user();
+    return view('backend.layouts.dashboard', compact('user'));
+})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/', function () {
+   
+    return view('frontend.app');
+})->name('home');
+
+require __DIR__.'/auth.php';
